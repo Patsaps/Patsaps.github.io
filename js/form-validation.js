9fb96ebf1a2b0fb579fcc2901e94140bfebf3706
Patsaps <patsaps9001@gmail.com>
2017-02-08 19:05:10 +1300

    Add files via upload
    First Form Validation

---
+++ js/form-validation.js
@@ -0,0 +1,42 @@
+﻿// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      details: "required",
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      phone: {
+        required: false,
+        minlength: 6
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      details: "Please specify what you want Mr Plumb and the crew to do for you",
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      phone: {
+        minlength: "Your phone number must be at least 5 numbers long"
+      },
+      email: "Please enter a valid email address"
+    },
+      
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
